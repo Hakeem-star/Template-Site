0b855d6015b4a35d8ea1da46050609fda7c57935
Hakeem-star <53916666+Hakeem-star@users.noreply.github.com>
2019-11-04 12:59:38 +0000

    Updates

---
+++ precache-manifest.7e91389d6500037da3112212416859ad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98871e63d27e81eca151f26b165ba238",
+    "url": "/adefe_hq/index.html"
+  },
+  {
+    "revision": "0412619f0c0064e2a972",
+    "url": "/adefe_hq/static/css/main.fd8acdcb.chunk.css"
+  },
+  {
+    "revision": "0bf06b29b55bc7c6e03a",
+    "url": "/adefe_hq/static/js/2.c6c7c927.chunk.js"
+  },
+  {
+    "revision": "0412619f0c0064e2a972",
+    "url": "/adefe_hq/static/js/main.c236c8e6.chunk.js"
+  },
+  {
+    "revision": "3bd9b9475b95169f6c63",
+    "url": "/adefe_hq/static/js/runtime-main.63cbc4bc.js"
+  },
+  {
+    "revision": "54d74ea9207fd6f9fcb974f680f8859b",
+    "url": "/adefe_hq/static/media/ARM_Business_Material_A5_Rectangle_33_pattern@2x.54d74ea9.png"
+  },
+  {
+    "revision": "9e49166a4e0341b934450448bc701572",
+    "url": "/adefe_hq/static/media/Adefe_HQ_Short_Web_A3_Rectangle_13_pattern@2x.9e49166a.png"
+  },
+  {
+    "revision": "39bf14b53603613d541475f3f36e3eb6",
+    "url": "/adefe_hq/static/media/DrukWide-Bold.39bf14b5.otf"
+  },
+  {
+    "revision": "0afbafb309f127b2226ab8bbe60e9dc5",
+    "url": "/adefe_hq/static/media/DrukWide-Medium.0afbafb3.otf"
+  },
+  {
+    "revision": "73b9994bb505da43a387d107849baf95",
+    "url": "/adefe_hq/static/media/MonumentExtended-Regular.73b9994b.otf"
+  },
+  {
+    "revision": "b49190a44726a4ec59b3b7c09cde179d",
+    "url": "/adefe_hq/static/media/MonumentExtended-Ultralight.b49190a4.otf"
+  }
+]);
