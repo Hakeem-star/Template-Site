b51c0db5d660927a3b9105e12e8d3ae9b8458d7d
Hakeem-star <53916666+Hakeem-star@users.noreply.github.com>
2019-11-25 21:11:23 +0000

    Scrollllll
    Work on scroll listener. Need to remove old listener and add a new one. Might need to make a new instance of the wheely function so it completely ignores the previous scrolls

---
+++ src/pages/functions/componentPositions.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+export default function componentPositions() {
+  //console.trace("called from");
+  let elementsOnPage = document.getElementById("mainContent").children;
+  let mainOffset = document.getElementById("mainContent").offsetTop;
+  let nav = document.getElementById("pageNavigation");
+  let navOffHeight = nav.offsetHeight;
+  let mainMargin =
+    getComputedStyle(document.getElementById("mainContent")).marginTop.slice(
+      0,
+      -2
+    ) * 1;
+  //get padding value
+  let navMargin =
+    getComputedStyle(
+      document.getElementById("pageNavigation")
+    ).marginBottom.slice(0, -2) * 1;
+  //console.log("right", mainOffset, nav, navMargin);
+  //console.log("elementsOnPage", elementsOnPage);
+  let mappedElementsOnPage = Array.prototype.slice
+    .call(elementsOnPage)
+    .map((e, i) => {
+      // console.log("test", e);
+      if (i !== 0) {
+        if (e.classList[1] === undefined && e.classList[1] !== "hide") {
+          // console.log(
+          //   "Offset",
+          //   e.offsetTop,
+          //   mainOffset,
+          //   navOffHeight,
+          //   mainMargin,
+          //   navMargin
+          // );
+          return e.offsetTop - mainMargin;
+        }
+      } else {
+        return e.offsetTop - mainMargin - navMargin;
+      }
+    });
+  let filtered = mappedElementsOnPage.filter(e => e !== undefined);
+  return [0, ...filtered, document.body.scrollHeight];
+}
--- src/pages/main.js
+++ src/pages/main.js
@@ -15,7 +15,7 @@ import Projects from "../pages/projects";
 import Newsletter from "../pages/newsletter";
 import logo from "../images/logos/Adefe_HQ_Short_Web_A3_Rectangle_13_pattern@2x.png";
 import content_image from "../images/left_section_images/ARM_Business_Material_A5_Rectangle_33_pattern@2x.png";
-
+import componentPositions from "./functions/componentPositions";
 //RightSection - this has padding. try considering
 
 //Calculate postion of elements. Need to add a buffer to center the component
@@ -37,9 +37,13 @@ class LeftSection extends Component {
   showImage() {
     // let timeOut = setTimeout(function() {
     console.log("testOut");
+
     document.addEventListener("scroll", () => {
       //console.log(window.scrollY > 500);
-      if (window.scrollY > 380 && window.scrollY < 600) {
+      if (
+        window.scrollY > componentPositions()[1] &&
+        window.scrollY < componentPositions()[3]
+      ) {
         this.setState({ imageDisplay: "showImage" });
       } else {
         this.setState({ imageDisplay: "" });
@@ -92,110 +96,68 @@ class RightSection extends Component {
       newsletterDisplay: 0,
       mainContentMarginTop: 0
     };
+    // this.currentElementCalc = this.currentElementCalc.bind(this);
+    // this.setState = this.setState.bind(this);
+    // componentPositions = componentPositions
   }
-  componentPositions() {
-    let elementsOnPage = document.getElementById("mainContent").children;
-    let mainOffset = document.getElementById("mainContent").offsetTop;
-    let nav = document.getElementById("pageNavigation");
-    let navOffHeight = nav.offsetHeight;
-    let mainMargin =
-      getComputedStyle(document.getElementById("mainContent")).marginTop.slice(
-        0,
-        -2
-      ) * 1;
-    //get padding value
-    let navMargin =
-      getComputedStyle(
-        document.getElementById("pageNavigation")
-      ).marginBottom.slice(0, -2) * 1;
-    console.log("right", mainOffset, nav, navMargin);
-    console.log("elementsOnPage", elementsOnPage);
-    let mappedElementsOnPage = Array.prototype.slice
-      .call(elementsOnPage)
-      .map((e, i) => {
-        //console.log("test", e);
-        if (i !== 0) {
-          if (e.classList[1] === undefined && e.classList[1] !== "hide") {
-            console.log(
-              "Offset",
-              e.offsetTop,
-              mainOffset,
-              navOffHeight,
-              navMargin
-            );
-            return (
-              e.offsetTop -
-              mainOffset -
-              navOffHeight -
-              mainMargin -
-              navMargin * 2
-            );
-          }
-        } else {
-          return mainOffset - navOffHeight;
-        }
-      });
-    let filtered = mappedElementsOnPage.filter(e => e !== undefined);
-    return [0, ...filtered, document.body.scrollHeight];
-  }
+
   componentDidMount(prevProps, prevState, snapshot) {
     // if (prevState.prevScroll !== this.state.prevScroll) {
     //window.clearTimeout(timeOut);
     //debugger;
-    const targetNode = document.getElementById("mainContent");
-    const config = { attributes: true, childList: false, subtree: false };
-    let that = this;
-    const callback = function(mutationsList, observer) {
-      // Use traditional 'for loops' for IE 11
-      console.log(that);
-      let mainOffset = document.getElementById("mainContent").offsetTop;
-      that.setState({
-        curEle: that.currentElementCalc(),
-        elePosition: that.componentPositions()
-      });
-      console.log(
-        getComputedStyle(mutationsList[0].target).marginTop,
-        mainOffset
-      );
-      for (let mutation of mutationsList) {
-        if (mutation.type === "childList") {
-          console.log("A child node has been added or removed.");
-        } else if (mutation.type === "attributes") {
-          console.log(
-            "The " + mutation.attributeName + " attribute was modified."
-          );
+    let testing = () => {
+      const targetNode = document.getElementById("mainContent");
+      const config = { attributes: true, childList: false, subtree: false };
+      const callback = (mutationsList, observer) => {
+        // Use traditional 'for loops' for IE 11
+        let mainOffset = document.getElementById("mainContent").offsetTop;
+        this.setState({
+          curEle: this.state.curEle,
+          elePosition: componentPositions()
+        });
+        // console.log(
+        //   mutationsList,
+        //   getComputedStyle(mutationsList[0].target).marginTop,
+        //   mainOffset
+        // );
+        for (let mutation of mutationsList) {
+          if (mutation.type === "childList") {
+            console.log("A child node has been added or removed.");
+          } else if (mutation.type === "attributes") {
+            console.log(
+              "The " + mutation.attributeName + " attribute was modified."
+            );
+          }
         }
-      }
+      };
+      // Create an observer instance linked to the callback function
+      const observer = new MutationObserver(callback);
+      // Start observing the target node for configured mutations
+      // observer.observe(targetNode, config);
     };
-
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver(callback);
-
-    // Start observing the target node for configured mutations
-    observer.observe(targetNode, config);
-
     //this.currentElementCalc();
-    console.log("mount", this.currentElementCalc(), this.componentPositions());
+    console.log(
+      "mount",
+      window.scrollY,
+      this.currentElementCalc(),
+      componentPositions()
+    );
     this.marginTopScrollListen();
     this.pageScrollListen();
     let elePositionCopy = this.state.elePosition.slice();
     elePositionCopy.push(document.body.scrollHeight);
     this.setState({
       curEle: this.currentElementCalc(),
-      elePosition: this.componentPositions()
+      elePosition: componentPositions()
     });
+
     // }
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    if (
-      prevState.elePosition !== this.state.elePosition ||
-      prevState.mainContentMarginTop !== this.state.mainContentMarginTop
-    ) {
+    if (prevState.curEle !== this.state.curEle) {
       // window.clearTimeout(timeOut);
       // this.timer();
-      debugger;
-      console.log("ELE", this.state.elePosition, this.state.curEle);
       const curEleCalc = () => {
         if (this.state.curEle === undefined) {
           return 0;
@@ -203,6 +165,13 @@ class RightSection extends Component {
           return this.state.elePosition[this.state.curEle];
         }
       };
+      console.log(
+        "ELE",
+        curEleCalc(),
+        this.state.elePosition,
+        this.state.curEle
+      );
+
       console.log("update", this.state);
       window.scrollTo({
         top: curEleCalc(),
@@ -215,7 +184,6 @@ class RightSection extends Component {
   currentElementBodyHeight() {
     let elementsOnPage = document.getElementById("mainContent").children;
     let mainOffset = document.getElementById("mainContent").offsetTop;
-
     //To create a scroll area for content,
     //calculate the height of the element and then add it from the offset top of the body
     let eleHeight = Array.slice.call(elementsOnPage).map(e => {
@@ -227,15 +195,16 @@ class RightSection extends Component {
     //console.trace("currentElementCalc", window.scrollY, this.state.elePosition);
     //This allows me to get the current element that should be in view when the page is loaded
     //Doing "less-than structure", which means we put the smaller numbers on top
-    if (window.scrollY < this.state.elePosition[1]) {
-      return 0;
-    } else if (window.scrollY < this.state.elePosition[2]) {
-      return 1;
-    } else if (window.scrollY < this.state.elePosition[3]) {
-      return 2;
-    } else {
-      return 0;
+    let ind = 0;
+    let compPos = componentPositions();
+    console.log(compPos);
+    for (let j = 0; j <= compPos.length; j++) {
+      console.log(this.state.elePosition.length);
+      if (window.scrollY < compPos[j]) {
+        return (ind = j);
+      }
     }
+    return ind;
   }
   counter = 0;
   pageScrollListen() {
@@ -246,49 +215,47 @@ class RightSection extends Component {
     // document.addEventListener("wheel", e => e.preventDefault(), {
     //   passive: false
     // });
-
+    let that = this;
     document.addEventListener(
       "wheel",
-      e => {
+      function wheely(e) {
+        console.log(e.path);
         e.preventDefault();
-        console.log(this.counter, "delta", e.deltaY, e.deltaX);
+        //console.log(this.counter, "delta", e.deltaY, e.deltaX);
 
         //the state check is async so it is fuffulling the condition multiple times. I need a sync variable to read and write to
         //Need a timer here to decrease the frequency of this event
         //Check if this.counter is 0
         //console.log(this.state.listenerDelayIndicator);
-        if (this.counter === 0) {
+
+        if (that.counter === 0) {
           //Assign the current scroll position to "current" variable
 
           //Change the value of this.counter to 1 to indicate this action has begun, so other events don't satisfy this condition
-          this.counter = 1;
+          that.counter = 1;
           //Clear timeout
-          clearTimeout(this.timeOut);
-          this.timeOut();
-          console.log("DOWn", this.state.curEle, this.state.elePosition.length);
+          clearTimeout(that.timeOut);
+          that.timeOut();
+          console.log("DOWn", that.state.curEle, that.state.elePosition.length);
           //delta indicates the direction the wheel is moving. Greater than 0 means down
           //If we are scrolling down and are not at the last element
           if (
             e.deltaY > 0 &&
-            this.state.curEle !== this.state.elePosition.length - 1
+            that.state.curEle !== that.state.elePosition.length - 1
           ) {
             //we have scrolled down
-            console.log("down");
+            //console.log("down");
             //cur -= 1;
             //increase the index this.counter for the current element
-
-            this.setState({
-              curEle: this.state.curEle + 1,
-              elePosition: this.componentPositions()
-
+            that.setState({
+              curEle: that.state.curEle + 1
               //listenerDelayIndicator: 1
             });
-          } else if (e.deltaY < 0 && this.state.curEle !== 0) {
+          } else if (e.deltaY < 0 && that.state.curEle !== 0) {
             console.log("up");
             //decrease the index this.counter for the current element
-            this.setState({
-              curEle: this.state.curEle - 1,
-              elePosition: this.componentPositions()
+            that.setState({
+              curEle: that.state.curEle - 1
               // listenerDelayIndicator: 1
             });
           }
@@ -311,6 +278,9 @@ class RightSection extends Component {
           //   prev = window.scrollY;
           //   this.setState({ curEle: this.state.curEle - 1 });
           // }
+
+          document.removeEventListener("wheel", wheely);
+          document.addEventListener("wheel", wheely);
         }
       },
       {
@@ -320,15 +290,20 @@ class RightSection extends Component {
   }
 
   timeOut = () => {
-    let that = this;
     // let cur = this.state.curEle;
-    setTimeout(function() {
+    setTimeout(() => {
       console.log("Doing");
-      that.counter = 0;
+      this.counter = 0;
       //that.setState({ listenerDelayIndicator: 0 }, () => {
       //clearTimeout(that.timeOut);
       //});
-    }, 1000);
+      console.log(componentPositions());
+      this.setState({
+        //curEle: this.state.curEle - 1,
+        elePosition: componentPositions()
+        // listenerDelayIndicator: 1
+      });
+    }, 1500);
   };
 
   marginTopScrollListen() {
