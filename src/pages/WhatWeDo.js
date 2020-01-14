a20581336578065e8635f100a04fe427b4b23b84
Hakeem-star <53916666+Hakeem-star@users.noreply.github.com>
2020-01-14 19:21:46 +0000

    Created the new layout but depending on plans for how the page should function, the site might need a rebuild which might actually be beneficial for functionality

--- src/pages/WhatWeDo.js
+++ src/pages/WhatWeDo.js
@@ -12,7 +12,7 @@ function WhatWeDo() {
       <div className="_content_container">
         <div className="_brand">
           <div className="_title">Brand Identity</div>
-          <div className="_dash">-</div>
+          <div className="_dash"></div>
           <div className="_article">
             Your brand is the cornerstone of your company our responsibility is
             to find unique and relevent truth about you, envelop ourselves in
@@ -28,18 +28,15 @@ function WhatWeDo() {
           <div className="_image">
             <img src="" alt="" />
           </div>
-          <div className="_dash">-</div>
+          <div className="_dash"></div>
           <div>
             <div className="_title"> Products & Services</div>
             <div className="_article">
-              Your brand is the cornerstone of your company our responsibility
-              is to find unique and relevent truth about you, envelop ourselves
-              in your story, and find the best way to henestly relate it to your
-              audience. From the creation of the identity to the development of
-              the brand system, our aim is to create a truly holistic brand
-              experience. Once we've created your logo, all facets of your brand
-              grow organically, eminating seamlessly and consistently through
-              all pieces of collateral and touch-points.
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus
+              iusto assumenda reiciendis minima at sint nulla beatae tempora
+              alias sequi ea deleniti culpa ipsum, officiis labore corporis
+              mollitia fuga voluptates deserunt. Illo ullam fugit tempora alias
+              exercitationem vel ratione molestiae!
             </div>
           </div>
         </div>
@@ -49,7 +46,7 @@ function WhatWeDo() {
       </div>
       <div className="_strategy">
         <div className="_title">Strategy</div>
-        <div className="_dash">-</div>
+        <div className="_dash"></div>
         <div className="_article">
           Consultation, thoughtful feedback, and redirection for existing
           brands. Maybe you have a stellar brand in place, but your brand
@@ -59,15 +56,16 @@ function WhatWeDo() {
         </div>
       </div>
       <div className="_partners">
-        <div className="_titles">
-          <div>Sales</div>
-          <div>Pr & Marketing</div>
-          <div>Video & Animation</div>
+        <div className="_labels">
+          <p className="sales">Sales</p> <div className="_window"></div>
         </div>
-        <div className="_windows">
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="_labels">
+          <p className="pr">Pr & Marketing</p>
+          <div className="_window"></div>
+        </div>
+        <div className="_labels">
+          <p className="video">Video & Animation</p>
+          <div className="_window"></div>
         </div>
       </div>
     </div>
